3019441285c0855d713204441b593de9569bd31d
Aitazaz <aitazazgilani@gmail.com>
2019-02-07 11:02:30 -0600

    first demo
    first demo

--- First assignment 01/sketch.js
+++ First assignment 01/sketch.js
@@ -5,11 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let x=45;
+let xspeed=6;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  fill(175,100,270);
+  ellipse(x,300,300,125);
+  rect(mouseX,400,75,75);
+  x += xspeed;
 }
