cfc8222f1c7cab5f5df68b42db8574b3fa2bd9bc
Aitazaz <aitazazgilani@gmail.com>
2019-02-12 11:01:49 -0600

    image demo
    demo

--- Primitive Paint/sketch.js
+++ Primitive Paint/sketch.js
@@ -7,15 +7,17 @@
 
 let x=45;
 let xspeed=6;
-function setup() {
+let randomRgb=[random(),random(),random()];
+function setup(){
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
 }
 
-function draw() {
+function draw(){
   background(255);
-  fill(175,100,270);
-  ellipse(x,300,300,125);
-  rect(mouseX,400,75,75);
-  x += xspeed;
+}
+function keyTyped(){
+  if (key==="a"){
+    rect(mouseX,mouseY,75,100);
+  }
 }
