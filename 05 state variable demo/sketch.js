a2cac2fb5fe0df90799ac62a457092b57bae42e8
Aitazaz <aitazazgilani@gmail.com>
2019-02-15 10:03:35 -0600

    image demo

---
+++ 05 state variable demo/sketch.js
@@ -0,0 +1,37 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let state = 0;
+let rectSize = 30;
+let x = 0;
+let y = 0;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  if (state === 0) {
+    x += 5;
+    if (x > windowWidth - rectSize) {
+      state = 1;
+    }
+  }
+  else if (state === 1) {
+    y += 5;
+    if (y > windowWidth - rectSize) {
+      state = 2;
+    }
+  }
+  else if (state === 2) {
+    x += -5;
+    if (x <=0) {
+      state = 3;
+    }
+  }
+  rect(x, y, rectSize, rectSize);
+}
--- Primitive Paint/sketch.js
+++ Primitive Paint/sketch.js
@@ -4,20 +4,40 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-let x=45;
-let xspeed=6;
-let randomRgb=[random(),random(),random()];
 function setup(){
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+  ellipseMode(CENTER);
 }
 
 function draw(){
-  background(255);
+  //background(255);
+  textSize(40);
+  textFont("Georgia");
+  text("Aitazaz", 10, 30);
 }
+
+function randomShape(){
+
+}
+
+
+
 function keyTyped(){
   if (key==="a"){
+    fill(random(0,355),random(0,355),random(0,355));
     rect(mouseX,mouseY,75,100);
   }
+  else if (key==="s"){
+    fill(random(0,355),random(0,355),random(0,355));
+    ellipse(mouseX,mouseY,100,100);
+  }
+  else if (key==="d"){
+    fill(random(0,355),random(0,355),random(0,355));
+    quad(mouseX, mouseY, 86, 20, 69, 63, 30, 76);
+  }
+  else if (key===" "){
+    background(255);
+  }
+ 
 }
