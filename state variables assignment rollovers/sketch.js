d8143982551f1737d94c8babd471cc4532d42d37
Aitazaz <aitazazgilani@gmail.com>
2019-02-26 11:01:56 -0600

    state variable assignment

--- state variables assignment rollovers/sketch.js
+++ state variables assignment rollovers/sketch.js
@@ -4,12 +4,26 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let mouseOver=false;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500,500);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
+  background(355);
+  line(250,0,250,500);
+  line(0,250,500,250);
+}
+function quad1(){
+  if (mouseX>=250 && mouseY<=250 ){
+    rect(250,0,250,250);
+    fill(0);
+    
+  }
+  else{
+    fill(355);
+    rect(250,0,250,250);
+  }
 }
